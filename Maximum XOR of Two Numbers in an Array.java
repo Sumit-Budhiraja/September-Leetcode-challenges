1e193c3b147e6be4fdac5518cd55fc4e7f71446b
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2020-09-17 00:55:47 +0530

    Add files via upload

---
+++ Maximum XOR of Two Numbers in an Array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        int max = 0;
+        for(int i = 0; i < nums.length - 1; i++)
+            for(int j = i + 1; j < nums.length; j++)
+                max = Math.max(max, nums[i] ^ nums[j]);
+        return max;
+    }
+}
