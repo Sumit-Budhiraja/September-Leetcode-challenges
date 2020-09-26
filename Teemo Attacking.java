4d89dc67aafa768578dc6e24d2a6208d7c4ad75e
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2020-09-27 01:06:59 +0530

    Add files via upload

---
+++ Teemo Attacking.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if(timeSeries.length == 0) return 0;
+        int totalTime = 0;
+        for(int i = 0; i < timeSeries.length - 1; i++)
+            totalTime += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
+        return totalTime + duration;
+    }
+}
