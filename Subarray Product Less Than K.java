57191603c3530fd92dd42a90ea242c5ff858c21c
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2020-09-29 00:40:00 +0530

    Add files via upload

---
+++ Subarray Product Less Than K.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        double logk = Math.log(k);
+        double[] prefix = new double[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i+1] = prefix[i] + Math.log(nums[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < prefix.length; i++) {
+            int lo = i + 1, hi = prefix.length;
+            while (lo < hi) {
+                int mi = lo + (hi - lo) / 2;
+                if (prefix[mi] < prefix[i] + logk - 1e-9) lo = mi + 1;
+                else hi = mi;
+            }
+            ans += lo - i - 1;
+        }
+        return ans;
+    }
+}
